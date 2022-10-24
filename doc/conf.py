41dc5c43766c008b67d90373bb387aab2d9f391e
Gu Yizhou <41227335+astroyzgu@users.noreply.github.com>
2022-10-24 15:19:46 +0800

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'csstmock'
-copyright = '2022, SJTU gropu'
+copyright = '2022, SJTU group'
 author = 'SJTU group'
 
 # The version info for the project you're documenting, acts as replacement for
